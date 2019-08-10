0126eb951c88c0a82ae5b6c2ac7f17587e5368dd
nrehder <nrehder@gmail.com>
2019-08-10 17:55:01 -0400

    updated all modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
-import { AppRoutingModule } from './app-routing.module';
-import { StoreModule } from '@ngrx/store'
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "../environments/environment";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SharedModule } from './shared/shared.module';
-import * as fromApp from './store/app.reducer'
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AuthEffects } from './auth/store/auth.effects';
-import { RecipeEffects } from './recipe-book/store/recipe.effects'
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { SharedModule } from "./shared/shared.module";
+import * as fromApp from "./store/app.reducer";
+import { AuthInterceptorService } from "./auth/auth-interceptor.service";
+import { AuthEffects } from "./auth/store/auth.effects";
+import { RecipeEffects } from "./recipe-book/store/recipe.effects";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects, RecipeEffects]),
-    StoreDevtoolsModule.instrument({logOnly:environment.production}),
-    SharedModule
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    SharedModule,
   ],
   providers: [
     {
-      provide:HTTP_INTERCEPTORS,
-      useClass:AuthInterceptorService,
-      multi:true}
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
